1e5a7ac2d2387d0fc79ee80dea7ca5977ca65685
Vini_sep1914 <vinicius.rondini@etec.sp.gov.br>
2024-11-19 12:02:41 -0300

    commit 4

--- calculo.py
+++ calculo.py
@@ -1,7 +1,7 @@
 x = float(input('digite o primeiro numero'))
 y = float(input('digite o segundo numero'))
 z = float(input('digite o terceiro numero'))
-media = ( x * 1 + y * 1 + z * 2) / 4
+media = ( x * 1 + y * 1 + z * 2) / (4)
 print(media)
 if media > 6 :
     print("aprovado")
