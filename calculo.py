358fda5708575c874ed284180b281e5e1e36c2b5
Vini_sep1914 <vinicius.rondini@etec.sp.gov.br>
2024-11-19 12:01:55 -0300

    Update calculo.py

--- calculo.py
+++ calculo.py
@@ -1,7 +1,7 @@
 x = float(input('digite o primeiro numero'))
 y = float(input('digite o segundo numero'))
 z = float(input('digite o terceiro numero'))
-media = ( x + y + z ) / 3 
+media = ( x * 1 + y * 1 + z * 2) / 4
 print(media)
 if media > 6 :
     print("aprovado")
