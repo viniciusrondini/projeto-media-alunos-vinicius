ad7b409e732e6c77291f7676449e2947e85c7266
Vini_sep1914 <vinicius.rondini@etec.sp.gov.br>
2024-11-19 09:13:44 -0300

    commit 2

--- calculo.py
+++ calculo.py
@@ -3,3 +3,7 @@ y = float(input('digite o segundo numero'))
 z = float(input('digite o terceiro numero'))
 media = ( x + y + z ) / 3 
 print(media)
+if media >= 6 :
+    print("aprovado")
+    else:
+        print("reprovado")
