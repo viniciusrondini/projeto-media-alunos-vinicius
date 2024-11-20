76190e5d98bbdcc4e404e448facf8811af78a9ee
Vini_sep1914 <vinicius.rondini@etec.sp.gov.br>
2024-11-19 22:33:37 -0300

    resolvendo problemas python

--- calculo.py
+++ calculo.py
@@ -1,10 +1,13 @@
-x = float(input('digite o primeiro numero'))
-y = float(input('digite o segundo numero'))
-z = float(input('digite o terceiro numero'))
-media = ( x * 1 + y * 1 + z * 2) / (4)
+x = float(input('digite o primeiro numero '))
+y = float(input('digite o segundo numero '))
+z = float(input('digite o terceiro numero '))
+media = (x * 1 + y * 1 + z * 2) / (4)
 print(media)
-if media > 6 :
+if media > 6:
     print("aprovado")
+else:
+    if media >= 5 <= 6:
+        print("recuperacao")
     else:
-        if media >= 5 and <= 6 :
-            print("recuperacao")
+        if media < 5 >= 0:
+            print("reprovado")
